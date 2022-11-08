83a21ce88a4e53bb1c6ef733a09515b7c8982cdf
Aniket Agarwal <aniketagarwal57@gmail.com>
2022-11-08 13:45:58 +0530

    added api endpoints for patent vitals

--- client/src/components/DoctorNavbar/DoctorNavbar.jsx
+++ client/src/components/DoctorNavbar/DoctorNavbar.jsx
@@ -7,7 +7,7 @@ import { useNavigate } from "react-router-dom";
 const DoctorNavbar = () => {
   const navigate = useNavigate();
   const handleHomeClick = () => {
-    navigate(`/`);
+    navigate(`/doctor/home`);
   };
   return (
     <>
--- client/src/redux/user/user.utils.jsx
+++ client/src/redux/user/user.utils.jsx
@@ -1,44 +1,44 @@
 import * as API from "../../api/index";
 
 export const setUserType = (type) => {
-	return type;
+  return type;
 };
 
 export const loginUser = async (username, password, userType) => {
-	const credentials = {
-		uid: username,
-		password: password,
-		user_type: userType,
-	};
+  const credentials = {
+    uid: username,
+    password: password,
+    user_type: userType,
+  };
 
-	let data = null;
-	await API.loginUser(credentials)
-		.then((response) => (data = response.data))
-		.catch((error) => console.log("error"));
-
-	return data;
+  let data = null;
+  await API.loginUser(credentials)
+    .then((response) => (data = response.data))
+    .catch((error) => console.log("error"));
+  console.log(data);
+  return data;
 };
 
 export const getUserProfile = async (uid, access_token) => {
-	let data = null;
-	await API.getEmployeeProfile(uid, access_token)
-		.then((response) => (data = response.data))
-		.catch((error) => console.log("error"));
+  let data = null;
+  await API.getEmployeeProfile(uid, access_token)
+    .then((response) => (data = response.data))
+    .catch((error) => console.log("error"));
 
-	return data;
+  return data;
 };
 
 export const getPatientHistory = async (access_token) => {
-	let data = null;
-	await API.getPatientHistory(access_token)
-		.then((response) => (data = response.data))
-		.catch((error) => console.log("error"));
+  let data = null;
+  await API.getPatientHistory(access_token)
+    .then((response) => (data = response.data))
+    .catch((error) => console.log("error"));
 
-	return data;
+  return data;
 };
 
 export const userLogout = () => {
-	return {
-		userType: "",
-	};
+  return {
+    userType: "",
+  };
 };
